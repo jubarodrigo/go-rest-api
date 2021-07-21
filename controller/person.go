40ebe5d7e82e92745a0434d9c1af9b838f67026b
Juba Rodrigo <you@example.com>
2021-07-21 09:22:16 -0300

    first commit

---
+++ controller/person.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/jubarodrigo/api-rest/model"
+)
+
+var people []model.Person
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+
+	people = append(people, model.Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &model.Address{City: "City X", State: "State X"}})
+	people = append(people, model.Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &model.Address{City: "City Z", State: "State Y"}})
+	people = append(people, model.Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+	json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
