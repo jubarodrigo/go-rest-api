f2e69ccf6e2a99c60f9b420f489b2034add211da
Juba Rodrigo <rgjuba@gmail.com>
2021-07-23 15:08:51 -0300

    alteracao em rotas

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
-	"github.com/jubarodrigo/api-rest/controller"
+	"github.com/jubarodrigo/api-rest/routes"
 )
 
-// função principal
+
 func main() {
-
-    router := mux.NewRouter()
-
-	router.HandleFunc("/", controller.GetPeople).Methods("GET")
-	router.HandleFunc("/{id}", controller.GetPerson).Methods("GET")
-	router.HandleFunc("/{id}",controller.CreatePerson).Methods("POST")
-	router.HandleFunc("/{id}", controller.DeletePerson).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":8088", router))
+	routes.Router()
 }
---
+++ routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jubarodrigo/api-rest/controller"
+)
+
+func Router() {
+
+    router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.GetPeople).Methods("GET")
+	router.HandleFunc("/{id}", controller.GetPerson).Methods("GET")
+	router.HandleFunc("/{id}",controller.CreatePerson).Methods("POST")
+	router.HandleFunc("/{id}", controller.DeletePerson).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8088", router))
+	
+}
